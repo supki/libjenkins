e3a9e6376e6f62759cb7b116bc4586e88de17ee4
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-09-07 18:40:50 +0400

    Concurrent queries.

--- src/Jenkins.hs
+++ src/Jenkins.hs
@@ -2,22 +2,21 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-{-# OPTIONS_GHC -W #-}
 module Jenkins where
 
+import           Control.Concurrent.Async (mapConcurrently)
 import           Control.Lens
 import           Control.Applicative (Applicative)
 import           Control.Monad.Free
-import           Control.Monad.Trans.Control (MonadBaseControl)
-import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Control (liftWith, restoreT)
+import           Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import           Data.Conduit (MonadResource)
+import           Data.Conduit (ResourceT)
 import           Network.HTTP.Conduit
+  (Manager, Request, withManager, applyBasicAuth, httpLbs, parseUrl, responseBody)
 import qualified Network.HTTP.Conduit.Lens as L
 
 import           Jenkins.REST.Method
@@ -29,11 +28,13 @@ newtype Jenk a = Jenk { unJenk :: Free JenkF a }
 
 data JenkF a =
     forall f. Get (Method f) (BL.ByteString -> a)
-  | Post (forall f. (Method f)) (BL.ByteString -> a)
+  | Post (forall f. Method f) (BL.ByteString -> a)
+  | forall b. Concurrently [Jenk b] ([b] -> a)
 
 instance Functor JenkF where
-  fmap f (Get  m g) = Get m  (f . g)
-  fmap f (Post m g) = Post m (f . g)
+  fmap f (Get  m g)          = Get  m (f . g)
+  fmap f (Post m g)          = Post m (f . g)
+  fmap f (Concurrently ms g) = Concurrently ms (f . g)
   {-# INLINE fmap #-}
 
 
@@ -43,6 +44,9 @@ get m = Jenk . liftF $ Get m id
 post :: (forall f. Method f) -> Jenk BL.ByteString
 post m = Jenk . liftF $ Post m id
 
+concurrently :: [Jenk a] -> Jenk [a]
+concurrently js = Jenk . liftF $ Concurrently js id
+
 
 type Host     = String
 type Port     = Int
@@ -51,13 +55,14 @@ type Password = B.ByteString
 
 
 withJenkins :: Host -> Port -> User -> Password -> Jenk a -> IO a
-withJenkins host port user password jenk = withManager $ \manager -> do
-  req <- liftIO $ parseUrl host
-  interpret manager (applyBasicAuth user password (req { port })) jenk
+withJenkins h p user password jenk = withManager $ \manager -> do
+  request <- liftIO $ parseUrl h
+  let request' = request
+        & L.port .~ p
+  interpret manager (applyBasicAuth user password request') jenk
 
 interpret
-  :: (io ~ IO, MonadBaseControl io m, MonadIO m, MonadResource m)
-  => Manager -> Request m -> Jenk a -> m a
+  :: Manager -> Request (ResourceT IO) -> Jenk a -> ResourceT IO a
 interpret manager request = iterM go . unJenk where
   go (Get m next) = do
     let request' = request
@@ -71,3 +76,6 @@ interpret manager request = iterM go . unJenk where
           & L.method .~ "POST"
     bs <- httpLbs request' manager
     next (responseBody bs)
+  go (Concurrently js next) = do
+    xs <- liftWith (\run -> mapConcurrently (run . interpret manager request) js) >>= mapM (restoreT . return)
+    next xs
