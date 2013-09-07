cda4e9821fc4a1b2aa6acc98faf801fb3b79bd40
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-09-07 16:42:15 +0400

    Split method encoding into Jenkins.REST.Method module.

--- src/Jenkins.hs
+++ src/Jenkins.hs
@@ -1,14 +1,11 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -W #-}
 module Jenkins where
 
@@ -18,19 +15,15 @@ import           Control.Monad.Free
 import           Control.Monad.Trans.Control (MonadBaseControl)
 import           Control.Monad.IO.Class
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
 import           Data.Conduit (MonadResource)
-import           Data.Monoid (Monoid(..), (<>))
-import           Data.String (IsString(..))
 import           Network.HTTP.Conduit
 import qualified Network.HTTP.Conduit.Lens as L
 
--- $setup
--- >>> :set -XOverloadedStrings
+import           Jenkins.REST.Method
 
 
-newtype Jenk a = Jenk { unJenk :: (Free JenkF a) }
+newtype Jenk a = Jenk { unJenk :: Free JenkF a }
   deriving (Functor, Applicative, Monad)
 
 
@@ -43,76 +36,18 @@ instance Functor JenkF where
   fmap f (Post m g) = Post m (f . g)
   {-# INLINE fmap #-}
 
-infix  6 :-, `as`
-infixr 7 :/
-data Method :: Format -> * where
-  Empty  :: Method a
-  Number :: Integer -> Method f
-  String :: String -> Method f
-  (:/)   :: Method f -> Method f -> Method f
-  (:-)   :: Method f -> As f -> Method f
-
-instance Num (Method f) where
-  (+)         = error "Method.(+): not supposed to be used"
-  (*)         = error "Method.(*): not supposed to be used"
-  abs         = error "Method.abs: not supposed to be used"
-  signum      = error "Method.signum: not supposed to be used"
-  fromInteger = Number
-
-instance IsString (Method f) where
-  fromString = String
-
-data Format = JSON | XML | Python
-
-data As :: Format -> * where
-  AsJSON   :: As JSON
-  AsXML    :: As XML
-  AsPython :: As Python
-
-(-/-) :: Method f -> Method f -> Method f
-(-/-) = (:/)
-
-as :: Method f -> As f -> Method f
-as = (:-)
-
-json :: As JSON
-json = AsJSON
-
-xml :: As XML
-xml = AsXML
-
-python :: As Python
-python = AsPython
-
-
--- |
---
--- >>> nicely ("build" :/ 7 :- AsXML)
--- "build/7/api/xml"
-nicely :: Method f -> String
-nicely Empty           = ""
-nicely (Number n)      = show n
-nicely (String s)      = s
-nicely (x :/ y)        = nicely x `combine` nicely y
-nicely (x :- AsJSON)   = nicely x `combine` "api" `combine` "json"
-nicely (x :- AsXML)    = nicely x `combine` "api" `combine` "xml"
-nicely (x :- AsPython) = nicely x `combine` "api" `combine` "python"
-
-combine :: (IsString m, Monoid m) => m -> m -> m
-combine x y = x <> "/" <> y
-
-
-type Host     = String
-type Port     = Int
-type User     = B.ByteString
-type Password = B.ByteString
-
 
 get :: Method f -> Jenk BL.ByteString
 get m = Jenk . liftF $ Get m id
 
 post :: (forall f. Method f) -> Jenk BL.ByteString
 post m = Jenk . liftF $ Post m id
+
+
+type Host     = String
+type Port     = Int
+type User     = B.ByteString
+type Password = B.ByteString
 
 
 withJenkins :: Host -> Port -> User -> Password -> Jenk a -> IO a
@@ -124,15 +59,15 @@ interpret
   :: (io ~ IO, MonadBaseControl io m, MonadIO m, MonadResource m)
   => Manager -> Request m -> Jenk a -> m a
 interpret manager request = iterM go . unJenk where
-  go (Get method next) = do
+  go (Get m next) = do
     let request' = request
-          & L.path   %~ (`combine` BC.pack (nicely method))
+          & L.path   %~ (`combine` render m)
           & L.method .~ "GET"
     bs <- httpLbs request' manager
     next (responseBody bs)
-  go (Post method next) = do
+  go (Post m next) = do
     let request' = request
-          & L.path   %~ (`combine` BC.pack (nicely method))
+          & L.path   %~ (`combine` render m)
           & L.method .~ "POST"
     bs <- httpLbs request' manager
     next (responseBody bs)
---
+++ src/Jenkins/REST/Method.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- | Jenkins RESTful API methods construction
+module Jenkins.REST.Method
+  ( -- * Types
+    Method, As, Format
+    -- * User interface helpers
+  , (-/-), as, json, xml, python
+    -- * Rendering
+  , render, combine
+  ) where
+
+import           Data.ByteString (ByteString)
+import           Data.Monoid (Monoid(..), (<>))
+import           Data.String (IsString(..))
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import           Data.Text (Text)
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+
+
+infix  6 :-, `as`
+infixr 7 :/
+-- | Jenkins RESTFul API method encoding
+data Method :: Format -> * where
+  Empty :: Method f
+  Text  :: Text -> Method f
+  (:/)  :: Method f -> Method f -> Method f
+  (:-)  :: Method f -> As f -> Method f
+
+instance Monoid (Method f) where
+  mempty  = Empty
+  mappend = (:/)
+
+-- | Only to support number literals
+instance Num (Method f) where
+  (+)         = error "Method.(+): not supposed to be used"
+  (*)         = error "Method.(*): not supposed to be used"
+  abs         = error "Method.abs: not supposed to be used"
+  signum      = error "Method.signum: not supposed to be used"
+  fromInteger = fromString . show
+
+instance IsString (Method f) where
+  fromString = Text . T.pack
+
+-- | Response formats
+data Format = JSON | XML | Python
+
+-- | Response format singleton type
+data As :: Format -> * where
+  AsJSON   :: As JSON
+  AsXML    :: As XML
+  AsPython :: As Python
+
+-- | Append 2 methods
+(-/-) :: Method f -> Method f -> Method f
+(-/-) = mappend
+
+-- | Choose response format
+as :: Method f -> As f -> Method f
+as = (:-)
+
+-- | JSON response format
+json :: As JSON
+json = AsJSON
+
+-- | XML response format
+xml :: As XML
+xml = AsXML
+
+-- | Python response format
+python :: As Python
+python = AsPython
+
+
+-- | Render 'Method' to something that can be sent over the wire
+--
+-- >>> render ("job" -/- 7 `as` xml)
+-- "job/7/api/xml"
+--
+-- >>> render ("job" -/- 7 `as` json)
+-- "job/7/api/json"
+--
+-- >>> render "restart"
+-- "restart"
+render :: Method f -> ByteString
+render Empty           = ""
+render (Text s)        = T.encodeUtf8 s
+render (x :/ y)        = render x `combine` render y
+render (x :- AsJSON)   = render x `combine` "api" `combine` "json"
+render (x :- AsXML)    = render x `combine` "api" `combine` "xml"
+render (x :- AsPython) = render x `combine` "api" `combine` "python"
+
+-- | Insert \"\/\" between two 'String'-like things and concat them.
+--
+-- >>> combine "foo" "bar"
+-- "foo/bar"
+combine :: (IsString m, Monoid m) => m -> m -> m
+combine x y = x <> "/" <> y
