90c6aafd1af554c5833e757d1e6c69df61691bf4
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-09-09 22:42:26 +0400

    Make the real executable from examples/Failed.hs

--- examples/Failed.hs
+++ examples/Failed.hs
@@ -1,23 +1,54 @@
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 -- | Show jobs which last build has failed
 module Main where
 
-import Control.Lens
-import Control.Lens.Aeson
-import Control.Monad (filterM)
-import Data.Text (Text)
-import Jenkins
-import Jenkins.REST.Method
-import Network.HTTP.Conduit (HttpException)
+import           Control.Lens
+import           Control.Lens.Aeson
+import           Control.Monad (filterM)
+import qualified Data.ByteString.Char8 as B
+import           Data.Text (Text)
+import qualified Data.Text.IO as T
+import           Jenkins
+import           Jenkins.REST.Method
+import           Network.HTTP.Conduit (HttpException)
+import           Options.Applicative
+import           System.Exit (exitFailure)
+
+{-# ANN module ("HLint: Use camelCase" :: String) #-}
 
 
-failed :: IO (Either HttpException [Text])
-failed = do
-  let user     = "__USER__"
-      password = "__PASSWORD__"
-  withJenkins "__JENKINS_URL__" __PORT__ user password $ do
-    res <- get ("" `as` json)
-    let jobs = res ^.. key "jobs"._Array.each.key "name"._String
-    flip filterM jobs $ \job -> do
-      res' <- get ("job" -/- text job `as` json)
-      return $ elemOf (key "color") "red" res'
+data Options = Options
+  { url      :: String
+  , port     :: Int
+  , user     :: B.ByteString
+  , password :: B.ByteString
+  }
+
+
+main :: IO ()
+main = do
+  opts <- customExecParser (prefs showHelpOnError) options
+  jobs <- find_failed opts
+  case jobs of
+    Right js -> mapM_ T.putStrLn js
+    Left  e  -> do
+      print e
+      exitFailure
+
+options :: ParserInfo Options
+options = info (helper <*> parser) fullDesc
+ where
+  parser = Options
+    <$> strOption (long "host" <> short 'h')
+    <*> option (long "port" <> short 'p')
+    <*> nullOption (reader (return . B.pack) <> long "user" <> short 'u')
+    <*> nullOption (reader (return . B.pack) <> long "token" <> short 't')
+
+find_failed :: Options -> IO (Either HttpException [Text])
+find_failed (Options { url, port, user, password }) = withJenkins url port user password $ do
+  res <- get ("" `as` json)
+  let jobs = res ^.. key "jobs"._Array.each.key "name"._String
+  flip filterM jobs $ \job -> do
+    res' <- get ("job" -/- text job `as` json)
+    return $ elemOf (key "color") "red" res'
