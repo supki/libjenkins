f04a5ebf8f86403fccf0a77a8f63969ea981c0fa
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-09-12 22:58:41 +0400

    Support Jenkins discovery API.

---
+++ examples/Discover.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | Discover Jenkins server on the network
+module Main where
+
+import           Data.Monoid ((<>), mempty) -- base
+import           Data.Text (Text)           -- text
+import qualified Data.Text as T             -- text
+import qualified Data.Text.IO as T          -- text
+import           Jenkins.Discover           -- libjenkins
+import           System.Exit (exitFailure)  -- base
+
+
+main :: IO ()
+main = do
+  -- send discover broadcast with 100ms timeout
+  discoveries <- discover 100000
+  case discoveries of
+    -- no Jenkins responded
+    [] -> exitFailure
+    -- pretty print responses
+    _  -> mapM_ (T.putStrLn . pretty) discoveries
+
+-- | Pretty print Jenkins discovery responses
+pretty :: Discover -> Text
+pretty x = T.unwords $
+  ["Jenkins", version x] ++ maybe mempty (return . between "(" ")") (server_id x) ++ ["at", url x]
+ where
+  between l r t = l <> t <> r
+
---
+++ src/Jenkins/Discover.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+-- | Discover Jenkins on the network
+module Jenkins.Discover
+  ( Discover(..), discover
+  ) where
+
+import           Control.Applicative (Applicative(..), (<$>))
+import           Control.Lens
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import           Data.Maybe (mapMaybe)
+import           Data.Text (Text)
+import           Network.BSD
+import           Network.Socket
+import           Network.Socket.ByteString as B
+import           System.Timeout (timeout)
+import qualified Text.XML as X
+import qualified Text.XML.Lens as X
+
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
+
+
+-- | Jenkins information
+data Discover = Discover
+  { version   :: Text
+  , url       :: Text
+  , server_id :: Maybe Text
+  } deriving (Show, Read)
+
+
+-- | Discover Jenkins on the network
+discover
+  :: Int           -- ^ timeout
+  -> IO [Discover]
+discover t = do
+  (b, addr) <- broadcastSocket
+  B.sendTo b (B.pack [0, 0, 0, 0]) addr -- does not matter what to send here
+
+  msgs <- while (timeout t (readAnswer b))
+
+  close b
+  return (mapMaybe parse msgs)
+ where
+  while :: IO (Maybe a) -> IO [a]
+  while io = go where
+    go = do
+      mr <- io
+      case mr of
+        Nothing -> return []
+        Just r  -> (r :) <$> go
+
+broadcastSocket :: IO (Socket, SockAddr)
+broadcastSocket = do
+  s <- getProtocolNumber "udp" >>= socket AF_INET Datagram
+  setSocketOption s Broadcast 1
+  return (s, SockAddrInet port (-1) {- 255.255.255.255 -})
+ where
+  port = 33848
+
+readAnswer :: Socket -> IO ByteString
+readAnswer s = fst <$> B.recvFrom s 4096
+
+
+-- | Parse Jenkins discovery response XML
+--
+-- The \"Scheme\" is as follows:
+--
+-- @
+-- <hudson>
+--   <version>...</version>
+--   <url>...</url>
+--   <server-id>...</server-id>
+-- </hudson>
+-- @
+parse :: ByteString -> Maybe Discover
+parse (X.parseLBS X.def . BL.fromStrict -> bs) = case bs of
+  Left  _      -> Nothing
+  Right parsed ->
+    let v = parsed^?deeper.X.el "version".content
+        u = parsed^?deeper.X.el "url".content
+        s = parsed^?deeper.X.el "server-id".content
+    in Discover <$> v <*> u <*> pure s
+ where
+  content = X.nodes.traverse.X._Content
+  deeper  = X.root.X.nodes.traverse.X._Element
