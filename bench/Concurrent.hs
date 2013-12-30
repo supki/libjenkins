ce17dc658f2e9e22fec727546b91f6f3badae7bb
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-12-30 14:50:54 +0400

    Initial concurrency benchmarks

---
+++ bench/Concurrent.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | Show jobs descriptions
+--
+-- Does all jobs API calls concurrently, otherwise is exactly like Sequential.hs
+module Main (main) where
+
+import           Control.Lens                 -- lens
+import           Control.Lens.Aeson           -- lens-aeson
+import qualified Data.ByteString.Char8 as B   -- bytestring
+import           Data.Text (Text)             -- text
+import           Jenkins.REST                 -- libjenkins
+import           System.Environment (getArgs) -- base
+import           System.Exit (exitFailure)    -- base
+
+
+main :: IO ()
+main = do
+  h:p:user:pass:_ <- getArgs
+  ds <- descriptions $
+    Settings (Host h) (Port (read p)) (User (B.pack user)) (APIToken (B.pack pass))
+  case ds of
+    Right ds' -> mapM_ print ds'
+    Left  e  -> do
+      print e
+      exitFailure
+
+descriptions :: Settings -> IO (Either Disconnect [Maybe Text])
+descriptions settings = runJenkins settings $ do
+  res <- get (json -?- "tree" -=- "jobs[name]")
+  let jobs = res ^.. key "jobs"._Array.each.key "name"._String
+  concurrentlys (map describe jobs)
+
+describe :: Text -> Jenkins (Maybe Text)
+describe name = do
+  desc <- get (job name `as` json -?- "tree" -=- "description")
+  return (desc ^? key "description"._String)
