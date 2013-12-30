e5723d9730ccf8f1490609e6cfb0dea25214191f
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-12-30 15:12:06 +0400

    Merge Concurrent.hs and Sequential.hs

--- bench/Concurrency.hs
+++ bench/Concurrency.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 -- | Show jobs descriptions
 --
--- Does all jobs API calls sequentially, otherwise is exactly like Concurrent.hs
+-- Does all jobs API calls concurrently, otherwise is exactly like Sequential.hs
 module Main (main) where
 
 import           Control.Lens                 -- lens
@@ -13,22 +14,29 @@ import           System.Environment (getArgs) -- base
 import           System.Exit (exitFailure)    -- base
 
 
+type Aggregate a b = (a -> Jenkins b) -> [a] -> Jenkins [b]
+
 main :: IO ()
 main = do
-  h:p:user:pass:_ <- getArgs
-  ds <- descriptions $
+  m:h:p:user:pass:_ <- getArgs
+  ds <- descriptions (aggregate m) $
     Settings (Host h) (Port (read p)) (User (B.pack user)) (APIToken (B.pack pass))
   case ds of
-    Right ds' -> mapM_ print ds'
+    Right ds -> mapM_ print ds
     Left  e  -> do
       print e
       exitFailure
+ where
+  aggregate :: String -> Aggregate a b
+  aggregate "concurrent" = (concurrentlys .) . map
+  aggregate "sequential" = mapM
+  aggregate _ = error "Unknown mode"
 
-descriptions :: Settings -> IO (Either Disconnect [Maybe Text])
-descriptions settings = runJenkins settings $ do
+descriptions :: Aggregate Text (Maybe Text) -> Settings -> IO (Either Disconnect [Maybe Text])
+descriptions aggregate settings = runJenkins settings $ do
   res <- get (json -?- "tree" -=- "jobs[name]")
   let jobs = res ^.. key "jobs"._Array.each.key "name"._String
-  mapM describe jobs
+  aggregate  describe jobs
 
 describe :: Text -> Jenkins (Maybe Text)
 describe name = do
