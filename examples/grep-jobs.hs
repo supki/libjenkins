18a6c36d516e280c94adf578fd0c7315e0e224f2
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-14 22:44:31 +0400

    Simpler example

---
+++ examples/grep-jobs.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- | Grep Jenkins instance jobs with Perl regex
+module Main (main) where
+
+import           Control.Lens                              -- lens
+import           Control.Exception.Lens                    -- lens
+import           Control.Lens.Aeson (key, values, _String) -- lens-aeson
+import           Control.Monad                             -- base
+import           Data.String (fromString)                  -- base
+import           Data.Text (Text)                          -- text
+import qualified Data.Text as Text                         -- text
+import qualified Data.Text.IO as Text                      -- text
+import           Jenkins.REST                              -- libjenkins
+  (ConnectInfo(..), _Value, runJenkins, get, json, (-?-), (-=-))
+import           System.Environment (getArgs)              -- base
+import           System.Exit.Lens                          -- lens
+import           System.Process (readProcessWithExitCode)  -- process
+import           Text.Printf (printf)                      -- base
+
+
+main :: IO ()
+main = do
+  user:pswd:host:port:regex:_ <- getArgs
+  jobs <- grep regex (ConnectInfo host (read port) (fromString user) (fromString pswd))
+  case jobs of
+    [] -> throwingM _ExitFailure 1
+    _  -> mapM_ Text.putStrLn jobs
+
+-- | Filter matching job names
+grep :: String -> ConnectInfo -> IO [Text]
+grep regex conn = do
+  jobs <- runJenkins conn $
+    get (json -?- "tree" -=- "jobs[name]") <&> \res -> res ^.. key "jobs".values.key "name"._String
+  filterM (match regex) (jobs ^.. _Value.folded)
+
+-- | Match job name again Perl regex
+match :: String -> Text -> IO Bool
+match regex name = do
+  (exitcode, _, _) <-
+    readProcessWithExitCode "perl" ["-n", "-e", printf "/%s/ or die" regex] (Text.unpack name)
+  return $ has _ExitSuccess exitcode
