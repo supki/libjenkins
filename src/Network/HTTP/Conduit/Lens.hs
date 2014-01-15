6049023723c4d883ec47588c5a5f7c47de33dcc0
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-16 00:02:55 +0400

    Complete Request lenses

--- src/Network/HTTP/Conduit/Lens.hs
+++ src/Network/HTTP/Conduit/Lens.hs
@@ -3,10 +3,23 @@
 -- | Optics for @http-conduit@ types
 module Network.HTTP.Conduit.Lens
   ( -- * 'Request' lenses
-    method, secure
-  , host, port
-  , path, queryString, requestBody, requestHeaders
-  , redirectCount, checkStatus, responseTimeout
+    method
+  , secure
+  , host
+  , port
+  , path
+  , queryString
+  , requestBody
+  , requestHeaders
+  , proxy
+  , hostAddress
+  , rawBody
+  , decompress
+  , redirectCount
+  , checkStatus
+  , responseTimeout
+  , cookieJar
+  , getConnectionWrapper
     -- * 'HttpException' prisms
   , AsHttpException(..)
   , _StatusCodeException
@@ -40,63 +53,102 @@ import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as Lazy
 import           Data.Word (Word64)
 import qualified Network.HTTP.Conduit as H
+import qualified Network.HTTP.Client.Internal as H
 import qualified Network.HTTP.Types as H
+import           Network.Socket (HostAddress)
 
 
 -- | 'H.method' lens
 method :: Lens' H.Request H.Method
-method f req = (\m' -> req { H.method = m' }) <$> f (H.method req)
+method f req = f (H.method req) <&> \m' -> req { H.method = m' }
 {-# INLINE method #-}
 
 -- | 'H.secure' lens
 secure :: Lens' H.Request Bool
-secure f req = (\s' -> req { H.secure = s' }) <$> f (H.secure req)
+secure f req = f (H.secure req) <&> \s' -> req { H.secure = s' }
 {-# INLINE secure #-}
 
 -- | 'H.host' lens
 host :: Lens' H.Request ByteString
-host f req = (\h' -> req { H.host = h' }) <$> f (H.host req)
+host f req = f (H.host req) <&> \h' -> req { H.host = h' }
 {-# INLINE host #-}
 
 -- | 'H.port' lens
 port :: Lens' H.Request Int
-port f req = (\p' -> req { H.port = p' }) <$> f (H.port req)
+port f req = f (H.port req) <&> \p' -> req { H.port = p' }
 {-# INLINE port #-}
 
 -- | 'H.path' lens
 path :: Lens' H.Request ByteString
-path f req = (\p' -> req { H.path = p' }) <$> f (H.path req)
+path f req = f (H.path req) <&> \p' -> req { H.path = p' }
 {-# INLINE path #-}
 
 -- | 'H.queryString' lens
 queryString :: Lens' H.Request ByteString
-queryString f req = (\qs' -> req { H.queryString = qs' }) <$> f (H.queryString req)
+queryString f req = f (H.queryString req) <&> \qs' -> req { H.queryString = qs' }
 {-# INLINE queryString #-}
 
 -- | 'H.requestBody' lens
 requestBody :: Lens' H.Request H.RequestBody
-requestBody f req = (\rb' -> req { H.requestBody = rb' }) <$> f (H.requestBody req)
+requestBody f req = f (H.requestBody req) <&> \rb' -> req { H.requestBody = rb' }
 {-# INLINE requestBody #-}
 
 -- | 'H.requestHeaders' lens
 requestHeaders :: Lens' H.Request H.RequestHeaders
-requestHeaders f req = (\rh' -> req { H.requestHeaders = rh' }) <$> f (H.requestHeaders req)
+requestHeaders f req = f (H.requestHeaders req) <&> \rh' -> req { H.requestHeaders = rh' }
 {-# INLINE requestHeaders #-}
+
+-- | 'H.proxy'
+proxy :: Lens' H.Request (Maybe H.Proxy)
+proxy f req = f (H.proxy req) <&> \mp' -> req { H.proxy = mp' }
+{-# INLINE proxy #-}
+
+-- | 'H.hostAddress'
+hostAddress :: Lens' H.Request (Maybe HostAddress)
+hostAddress f req = f (H.hostAddress req) <&> \ha' -> req { H.hostAddress = ha' }
+{-# INLINE hostAddress #-}
+
+-- | 'H.rawBody'
+rawBody :: Lens' H.Request Bool
+rawBody f req = f (H.rawBody req) <&> \b' -> req { H.rawBody = b' }
+{-# INLINE rawBody #-}
+
+-- | 'H.decompress'
+decompress :: Lens' H.Request (ByteString -> Bool)
+decompress f req = f (H.decompress req) <&> \btb' -> req { H.decompress = btb' }
+{-# INLINE decompress #-}
 
 -- | 'H.redirectCount' lens
 redirectCount :: Lens' H.Request Int
-redirectCount f req = (\rc' -> req { H.redirectCount = rc' }) <$> f (H.redirectCount req)
+redirectCount f req = f (H.redirectCount req) <&> \rc' -> req { H.redirectCount = rc' }
 {-# INLINE redirectCount #-}
 
 -- | 'H.checkStatus' lens
 checkStatus :: Lens' H.Request (H.Status -> H.ResponseHeaders -> H.CookieJar -> Maybe SomeException)
-checkStatus f req = (\cs' -> req { H.checkStatus = cs' }) <$> f (H.checkStatus req)
+checkStatus f req = f (H.checkStatus req) <&> \cs' -> req { H.checkStatus = cs' }
 {-# INLINE checkStatus #-}
 
 -- | 'H.responseTimeout' lens
 responseTimeout :: Lens' H.Request (Maybe Int)
-responseTimeout f req = (\rt' -> req { H.responseTimeout = rt' }) <$> f (H.responseTimeout req)
+responseTimeout f req = f (H.responseTimeout req) <&> \rt' -> req { H.responseTimeout = rt' }
 {-# INLINE responseTimeout #-}
+
+-- | 'H.cookieJar'
+cookieJar :: Lens' H.Request (Maybe H.CookieJar)
+cookieJar f req = f (H.cookieJar req) <&> \mcj' -> req { H.cookieJar = mcj' }
+{-# INLINE cookieJar #-}
+
+-- | 'H.getConnectionWrapper'
+getConnectionWrapper
+  :: Lens' H.Request
+      ( Maybe Int
+      -> H.HttpException
+      -> IO (H.ConnRelease, H.Connection, H.ManagedConn)
+      -> IO (Maybe Int, (H.ConnRelease, H.Connection, H.ManagedConn))
+      )
+getConnectionWrapper f req =
+  f (H.getConnectionWrapper req) <&> \wat' -> req { H.getConnectionWrapper = wat' }
+{-# INLINE getConnectionWrapper #-}
 
 
 -- | @http-conduit@ exceptions
