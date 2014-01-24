c8d5c9edefe011b7e5d34930ba9942e92bcbb7dc
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-25 02:27:11 +0400

    Simplify exceptions overloading

--- src/Network/HTTP/Conduit/Lens.hs
+++ src/Network/HTTP/Conduit/Lens.hs
@@ -45,7 +45,6 @@ module Network.HTTP.Conduit.Lens
   , _IncompleteHeaders
   ) where
 
-import           Control.Applicative
 import           Control.Exception (SomeException, IOException)
 import           Control.Exception.Lens (exception)
 import           Control.Lens
@@ -152,112 +151,90 @@ getConnectionWrapper f req =
 
 
 -- | @http-conduit@ exceptions
-class AsHttpException p f t where
+class AsHttpException t where
   -- | @http-conduit@ exceptions overloading
-  _HttpException :: Overloaded' p f t H.HttpException
+  _HttpException :: Prism' t H.HttpException
 
-instance AsHttpException p f H.HttpException where
+instance AsHttpException H.HttpException where
   _HttpException = id
   {-# INLINE _HttpException #-}
 
-instance (Choice p, Applicative f) => AsHttpException p f SomeException where
+instance AsHttpException SomeException where
   _HttpException = exception
   {-# INLINE _HttpException #-}
 
 -- | 'H.StatusCodeException' exception
-_StatusCodeException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (H.Status, H.ResponseHeaders, H.CookieJar)
+_StatusCodeException :: AsHttpException t => Prism' t (H.Status, H.ResponseHeaders, H.CookieJar)
 _StatusCodeException = _HttpException . prism' (uncurry3 H.StatusCodeException) go where
   go (H.StatusCodeException s rh cj) = Just (s, rh, cj)
   go _ = Nothing
 {-# INLINE _StatusCodeException #-}
 
 -- | 'H.InvalidUrlException' exception
-_InvalidUrlException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (String, String)
+_InvalidUrlException :: AsHttpException t => Prism' t (String, String)
 _InvalidUrlException = _HttpException . prism' (uncurry H.InvalidUrlException) go where
   go (H.InvalidUrlException s s') = Just (s, s')
   go _ = Nothing
 {-# INLINE _InvalidUrlException #-}
 
 -- | 'H.TooManyRedirects' exception
-_TooManyRedirects
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t [H.Response Lazy.ByteString]
+_TooManyRedirects :: AsHttpException t => Prism' t [H.Response Lazy.ByteString]
 _TooManyRedirects = _HttpException . prism' H.TooManyRedirects go where
   go (H.TooManyRedirects rs) = Just rs
   go _ = Nothing
 {-# INLINE _TooManyRedirects #-}
 
 -- | 'H.UnparseableRedirect' exception
-_UnparseableRedirect
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (H.Response Lazy.ByteString)
+_UnparseableRedirect :: AsHttpException t => Prism' t (H.Response Lazy.ByteString)
 _UnparseableRedirect = _HttpException . prism' H.UnparseableRedirect go where
   go (H.UnparseableRedirect r) = Just r
   go _ = Nothing
 {-# INLINE _UnparseableRedirect #-}
 
 -- | 'H.TooManyRetries' exception
-_TooManyRetries
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_TooManyRetries :: AsHttpException t => Prism' t ()
 _TooManyRetries = _HttpException . prism' (const H.TooManyRetries) go where
   go H.TooManyRetries = Just ()
   go _ = Nothing
 {-# INLINE _TooManyRetries #-}
 
 -- | 'H.HttpParserException' exception
-_HttpParserException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t String
+_HttpParserException :: AsHttpException t => Prism' t String
 _HttpParserException = _HttpException . prism' H.HttpParserException go where
   go (H.HttpParserException s) = Just s
   go _ = Nothing
 {-# INLINE _HttpParserException #-}
 
 -- | 'H.HandshakeFailed' exception
-_HandshakeFailed
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_HandshakeFailed :: AsHttpException t => Prism' t ()
 _HandshakeFailed = _HttpException . prism' (const H.HandshakeFailed) go where
   go H.HandshakeFailed = Just ()
   go _ = Nothing
 {-# INLINE _HandshakeFailed #-}
 
 -- | 'H.OverlongHeaders' exception
-_OverlongHeaders
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_OverlongHeaders :: AsHttpException t => Prism' t ()
 _OverlongHeaders = _HttpException . prism' (const H.OverlongHeaders) go where
   go H.OverlongHeaders = Just ()
   go _ = Nothing
 {-# INLINE _OverlongHeaders #-}
 
 -- | 'H.ResponseTimeout' exception
-_ResponseTimeout
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_ResponseTimeout :: AsHttpException t => Prism' t ()
 _ResponseTimeout = _HttpException . prism' (const H.ResponseTimeout) go where
   go H.ResponseTimeout = Just ()
   go _ = Nothing
 {-# INLINE _ResponseTimeout #-}
 
 -- | 'H.FailedConnectionException' exception
-_FailedConnectionException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (String, Int)
+_FailedConnectionException :: AsHttpException t => Prism' t (String, Int)
 _FailedConnectionException = _HttpException . prism' (uncurry H.FailedConnectionException) go where
   go (H.FailedConnectionException s i) = Just (s, i)
   go _ = Nothing
 {-# INLINE _FailedConnectionException #-}
 
 -- | 'H.ExpectedBlankAfter100Continue' exception
-_ExpectedBlankAfter100Continue
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_ExpectedBlankAfter100Continue :: AsHttpException t => Prism' t ()
 _ExpectedBlankAfter100Continue =
   _HttpException . prism' (const H.ExpectedBlankAfter100Continue) go where
     go H.ExpectedBlankAfter100Continue = Just ()
@@ -265,90 +242,70 @@ _ExpectedBlankAfter100Continue =
 {-# INLINE _ExpectedBlankAfter100Continue #-}
 
 -- | 'H.InvalidStatusLine' exception
-_InvalidStatusLine
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ByteString
+_InvalidStatusLine :: AsHttpException t => Prism' t ByteString
 _InvalidStatusLine = _HttpException . prism' H.InvalidStatusLine go where
   go (H.InvalidStatusLine b) = Just b
   go _ = Nothing
 {-# INLINE _InvalidStatusLine #-}
 
 -- | 'H.InvalidHeader' exception
-_InvalidHeader
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ByteString
+_InvalidHeader :: AsHttpException t => Prism' t ByteString
 _InvalidHeader = _HttpException . prism' H.InvalidHeader go where
   go (H.InvalidHeader b) = Just b
   go _ = Nothing
 {-# INLINE _InvalidHeader #-}
 
 -- | 'H.InternalIOException' exception
-_InternalIOException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t IOException
+_InternalIOException :: AsHttpException t => Prism' t IOException
 _InternalIOException = _HttpException . prism' H.InternalIOException go where
   go (H.InternalIOException ioe) = Just ioe
   go _ = Nothing
 {-# INLINE _InternalIOException #-}
 
 -- | 'H.ProxyConnectException' exception
-_ProxyConnectException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (ByteString, Int, Either ByteString H.HttpException)
+_ProxyConnectException :: AsHttpException t => Prism' t (ByteString, Int, Either ByteString H.HttpException)
 _ProxyConnectException = _HttpException . prism' (uncurry3 H.ProxyConnectException) go where
   go (H.ProxyConnectException b i ebhe) = Just (b, i, ebhe)
   go _ = Nothing
 {-# INLINE _ProxyConnectException #-}
 
 -- | 'H.NoResponseDataReceived' exception
-_NoResponseDataReceived
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_NoResponseDataReceived :: AsHttpException t => Prism' t ()
 _NoResponseDataReceived = _HttpException . prism' (const H.NoResponseDataReceived) go where
   go H.NoResponseDataReceived = Just ()
   go _ = Nothing
 {-# INLINE _NoResponseDataReceived #-}
 
 -- | 'H.TlsException' exception
-_TlsException
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t SomeException
+_TlsException :: AsHttpException t => Prism' t SomeException
 _TlsException = _HttpException . prism' H.TlsException go where
   go (H.TlsException se) = Just se
   go _ = Nothing
 {-# INLINE _TlsException #-}
 
 -- | 'H.TlsNotSupported' exception
-_TlsNotSupported
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_TlsNotSupported :: AsHttpException t => Prism' t ()
 _TlsNotSupported = _HttpException . prism' (const H.TlsNotSupported) go where
   go H.TlsNotSupported = Just ()
   go _ = Nothing
 {-# INLINE _TlsNotSupported #-}
 
 -- | 'H.ResponseBodyTooShort' exception
-_ResponseBodyTooShort
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t (Word64, Word64)
+_ResponseBodyTooShort :: AsHttpException t => Prism' t (Word64, Word64)
 _ResponseBodyTooShort = _HttpException . prism' (uncurry H.ResponseBodyTooShort) go where
   go (H.ResponseBodyTooShort w w') = Just (w, w')
   go _ = Nothing
 {-# INLINE _ResponseBodyTooShort #-}
 
 -- | 'H.InvalidChunkHeaders' exception
-_InvalidChunkHeaders
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_InvalidChunkHeaders :: AsHttpException t => Prism' t ()
 _InvalidChunkHeaders = _HttpException . prism' (const H.InvalidChunkHeaders) go where
   go H.InvalidChunkHeaders = Just ()
   go _ = Nothing
 {-# INLINE _InvalidChunkHeaders #-}
 
 -- | 'H.IncompleteHeaders' exception
-_IncompleteHeaders
-  :: (AsHttpException p f t, Choice p, Applicative f)
-  => Overloaded' p f t ()
+_IncompleteHeaders :: AsHttpException t => Prism' t ()
 _IncompleteHeaders = _HttpException . prism' (const H.IncompleteHeaders) go where
   go H.IncompleteHeaders = Just ()
   go _ = Nothing
