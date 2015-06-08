4902d087bb1fd7a574504eb005f34b109b8b7000
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-06-08 14:01:26 +0000

    Modernize Nix configuration

--- default.nix
+++ default.nix
@@ -1,20 +1,2 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
-
-haskellPackages.cabal.mkDerivation (self: rec {
-  pname = "libjenkins";
-  version = "0.8.0";
-  src = builtins.filterSource (path: type: type != "unknown") ./.;
-  buildDepends = with haskellPackages; [
-    attoparsec free httpClient httpTypes httpConduit liftedAsync
-    liftedBase monadControl mtl network networkUri profunctors
-    text transformers
-  ];
-  testDepends = with haskellPackages; buildDepends ++ [
-    async doctest filepath hspec hspecExpectationsLens lens xmlConduit
-  ];
-  meta = {
-    description = "Jenkins API interface";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./libjenkins.nix {}
