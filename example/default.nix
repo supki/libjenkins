a087c3445ddbfe829af48a76a0730306e083a94b
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-08-27 12:49:52 +0000

    Use the local version of the library in examples.

--- example/default.nix
+++ example/default.nix
@@ -1,2 +1,4 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./libjenkins-examples.nix {}
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./libjenkins-examples.nix {
+  libjenkins = import ../. { inherit nixpkgs compiler; };
+}
